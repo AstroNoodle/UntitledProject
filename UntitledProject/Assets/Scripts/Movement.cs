5c971f168005d93743bb2440b702edd0121a43e4
Chion0phile <60484852+Chion0phile@users.noreply.github.com>
2021-03-01 18:00:38 +0100

    Test #1

---
+++ UntitledProject/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    private float HorizontalInput;
+    private float VerticalInput;
+    public float MovementSpeed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+            rb.velocity = new Vector3(HorizontalInput * MovementSpeed, rb.velocity.y, VerticalInput * MovementSpeed);
+    }
+}
