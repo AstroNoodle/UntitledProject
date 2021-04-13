3981a21cbf06253ec8334708cde49542a95ff380
Chion0phile <Chion0phile@users.noreply.github.com>
2021-04-13 19:01:39 +0200

    2'nd player

---
+++ UntitledProject/Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPosition : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject Player1;
+    public float speed;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 player = gameObject.transform.position;
+    }
+}
--- UntitledProject/Assets/Scripts/Movement.cs
+++ UntitledProject/Assets/Scripts/Movement.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Rigidbody rb;
-    private float HorizontalInput;
-    private float VerticalInput;
+    public GameObject CamTarget;
+    public GameObject Player;
+    public GameObject Player1;
+    public Rigidbody rb;
+    public Rigidbody rb1;
     public float MovementSpeed;
     public float DownForce;
     public float DownForceRayLenght;
+    public float MaxDistance;
+    public List<Transform> Targets;
+    private float distance;
+    private float HorizontalInput;
+    private float VerticalInput;
+    private float HorizontalInput1;
+    private float VerticalInput1;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        rb = Player.GetComponent<Rigidbody>();
+        rb1 = Player1.GetComponent<Rigidbody>();
     }
     private void FixedUpdate()
     {
         PlayerMovement();
+        Player1Movement();
+        if (Targets.Count == 0)
+            return;
+        Vector3 CenterPoint = GetCenterPoint();
+        CamTarget.transform.position = CenterPoint;
+        distance = Vector3.Distance(Player.transform.position, Player1.transform.position);
     }
 
     private void PlayerMovement()
@@ -30,14 +46,59 @@ public class Movement : MonoBehaviour
         {
             rb.velocity = new Vector3(HorizontalInput * MovementSpeed, rb.velocity.y + DownForce, VerticalInput * MovementSpeed);
         }
+
+        if(distance > MaxDistance)
+        {
+            //Player.transform.position = new Vector3(Mathf.Clamp(Player.transform.position.x, distance, MaxDistance), Player.transform.position.y, Player.transform.position.z);
+        }
+    }
+
+    private void Player1Movement()
+    {
+        HorizontalInput1 = Input.GetAxis("Horizontal1");
+        VerticalInput1 = Input.GetAxis("Vertical1");
+        rb1.velocity = new Vector3(HorizontalInput1 * MovementSpeed, rb1.velocity.y, VerticalInput1 * MovementSpeed);
+
+        if ((HorizontalInput1 != 0 || VerticalInput1 != 0) && OnSlope1())
+        {
+            rb1.velocity = new Vector3(HorizontalInput1 * MovementSpeed, rb1.velocity.y + DownForce, VerticalInput1 * MovementSpeed);
+        }
+        if (distance > MaxDistance)
+        {
+            //Player1.transform.position = new Vector3(Mathf.Clamp(Player1.transform.position.x, distance, MaxDistance), Player1.transform.position.y, Player1.transform.position.z);
+        }
     }
 
     private bool OnSlope()
     {
         RaycastHit Hit;
-        if (Physics.Raycast(transform.position, Vector3.down, out Hit, DownForceRayLenght))
+        if (Physics.Raycast(Player.transform.position, Vector3.down, out Hit, DownForceRayLenght))
             if (Hit.normal != Vector3.up)
                 return true;
-        return false;
+            return false;
+    }
+
+    private bool OnSlope1()
+    {
+        RaycastHit Hit1;
+        if (Physics.Raycast(Player1.transform.position, Vector3.down, out Hit1, DownForceRayLenght))
+            if (Hit1.normal != Vector3.up)
+                return true;
+            return false;
+    }
+
+    Vector3 GetCenterPoint()
+    {
+        if (Targets.Count == 1)
+        {
+            return Targets[0].position;
+        }
+
+        var bounds = new Bounds(Targets[0].position, Vector3.zero);
+        for (int i = 0; i < Targets.Count; i++)
+        {
+            bounds.Encapsulate(Targets[i].position);
+        }
+        return bounds.center;
     }
 }
